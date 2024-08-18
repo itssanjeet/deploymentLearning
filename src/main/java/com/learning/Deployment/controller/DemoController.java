af8f82ff1f61401daf13555c6653eb29a4ea8edb
sanjeet.kumar <sanjeet.kumar@smartenspaces.com>
2024-08-18 15:09:05 +0530

    changed return type

--- src/main/java/com/learning/Deployment/controller/DemoController.java
+++ src/main/java/com/learning/Deployment/controller/DemoController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("demo")
 public class DemoController {
 
-    @GetMapping("hello")
-    public String getStringValue(){
-        return "Hello world!";
+    @GetMapping
+    public Object getStringValue(){
+        Map<String, String> hm = new HashMap<>();
+        hm.put("name", "Sanjeet Kumar");
+        hm.put("address", "Somewhere");
+        return hm;
     }
 }
