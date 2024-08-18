e0665556855ffeaf815b8c011d9106ee0198de31
sanjeet.kumar <sanjeet.kumar@smartenspaces.com>
2024-08-18 14:50:45 +0530

    Initial commit

---
+++ src/main/java/com/learning/Deployment/DeploymentLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.Deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentLearningApplication.class, args);
+	}
+
+}
